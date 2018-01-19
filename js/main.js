f6e22c58ba7a95ae210c0e97932b0887cd09cfd4
Karthik M S <ms18karthik@gmail.com>
2018-01-19 19:28:47 +0530

    Added the error handling to the AJAX request

--- js/main.js
+++ js/main.js
@@ -9,19 +9,53 @@ btn.addEventListener("click", function(){
 	var ourRequest = new XMLHttpRequest();
 	ourRequest.open('GET', 'https://raw.githubusercontent.com/LearnWebCode/json-example/master/animals-' +pageCounter+ '.json');
 	ourRequest.onload = function () {
-	var ourData = JSON.parse(ourRequest.responseText);
-	renderHTML(ourData);
+
+		if (ourRequest.status >= 200 && ourRequest.status < 400) {
+			var ourData = JSON.parse(ourRequest.responseText);
+			renderHTML(ourData);
+		}else {
+			console.log("We connected to the server but the server returned an error.")
+		}
 	};
+
+	ourRequest.onerror = function () {
+		console.log("Connection error");
+	}
 
 	ourRequest.send();
 	pageCounter++;
+
+	if (pageCounter > 3) {
+		btn.classList.add("hide-me");
+	}
 });
 
 function renderHTML(data) {
 	var htmlString = "";
 
 	for (i = 0; i < data.length; i++) {
-		htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>" ;
+		htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat " ;
+
+		for (j = 0; j < data[i].foods.likes.length; j++) {
+			if (j == 0) {
+				htmlString += data[i].foods.likes[j];
+			}else {
+				htmlString += " and " + data[i].foods.likes[j];
+			}
+		}
+
+		htmlString += ' and dislikes ';
+
+
+		for (j = 0; j < data[i].foods.dislikes.length; j++) {
+			if (j == 0) {
+				htmlString += data[i].foods.dislikes[j];
+			}else {
+				htmlString += " and " + data[i].foods.dislikes[j];
+			}
+		}
+
+		htmlString += '.</p>';
 	}
 
 	animalContainer.insertAdjacentHTML('beforeend', htmlString);
